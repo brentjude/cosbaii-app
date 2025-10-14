167b3c053b3f81075f161d995e2ef6546993a1af
Jude <60078533+benzomai@users.noreply.github.com>
2025-10-14 11:24:44 +0800

    fixed css on roadmap section

--- src/app/components/home/Roadmap.tsx
+++ src/app/components/home/Roadmap.tsx
@@ -5,8 +5,8 @@ const Roadmap = () => {
     <section className="w-full bg-[#F6F6F6] py-16 sm:py-24 px-8">
         <div className="max-w-[1440px] mx-auto text-center">
             <h2 className="font-paytone color-cosbaii-primary text-[32px] sm:text-[64px]">Cosbaii in the future</h2>
-            <p className="w-full sm:w-[826px] mx-auto text-[20px] mt-8">Cosbaii isn&apos;t just a platform. It&apos;s a growing space built with and for the cosplay community. From exciting features to meaningful opportunities, here&apos;s a peek at what&apos;s coming next, designed to make your cosplay journey more connected, creative, and fun.</p>
-            <div className="flex flex-col sm:flex-row gap-8 mt-12">
+            <p className="w-full sm:w-[826px] mx-auto text-[20px] mt-4">Cosbaii isn&apos;t just a platform. It&apos;s a growing space built with and for the cosplay community. From exciting features to meaningful opportunities, here&apos;s a peek at what&apos;s coming next, designed to make your cosplay journey more connected, creative, and fun.</p>
+            <div className="flex flex-col gap-8 mt-12">
                 <div className="flex flex-col sm:flex-row gap-8">
                     <div className="w-full sm:w-[50%] bg-[#FFF2E8] text-left rounded-2xl px-8 py-6">
                         <h3 className="text-[24px] sm:text-[32px] font-black">For Cosplayers, Organizers, Photographers & Fans</h3>
